433a18aa0707cdcaf09e948804e52db173b0f30d
JuanCigales <cigales.juan.mrm@gmail.com>
2024-07-10 12:49:28 +0200

    Validacion del formulario

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,42 +1,86 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Create New Contact</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create New Contact</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('contacts.store') }}">
-                        @csrf
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('contacts.store') }}">
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                                <div class="col-md-6">
+                                    <input id="name"
+                                        type="text"class="form-control @error('name') is-invalid @enderror"
+                                        name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
+                            
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Phone Number</label>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-
-                            <div class="col-md-6">
-                                <input id="phone_number" type="tel" class="form-control" name="phone_number" required autocomplete="phone_number">
+                                <div class="col-md-6">
+                                    <input id="phone_number" type="tel"
+                                        class="@error('phone_number') is-invalid @enderror form-control" name="phone_number" value="{{ old('phone_number') }}"
+                                        autocomplete="phone_number">
+                                    @error('phone_number')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Submit
-                                </button>
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="text"
+                                        class="@error('email') is-invalid @enderror form-control" name="email" value="{{ old('email') }}"
+                                        autocomplete="email">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Age</label>
+
+                                <div class="col-md-6">
+                                    <input id="age" type="text"
+                                        class="@error('age') is-invalid @enderror form-control" name="age" value="{{ old('age') }}"
+                                        autocomplete="age">
+                                    @error('age')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
