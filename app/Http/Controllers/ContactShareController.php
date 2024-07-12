cb0eba4ba64ce2a4d4119afe841dcf293a74d991
JuanCigales <cigales.juan.mrm@gmail.com>
2024-07-12 10:09:57 +0200

    Contact share

---
+++ app/Http/Controllers/ContactShareController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ContactShareController extends Controller
+{
+    public function create()
+    {
+        return view('contact-shares.create');
+    }
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'user_email' => "exists:users,email|not_in:{$request->user()->email}",
+            'contact_email' => Rule::exists('contacts', 'email')
+                ->where('user_id', auth()->id()),
+        ], [
+            'user_email.not_in' => "You cant share a contact with yourself",
+            'contact_email.exists' => "This contact was not found in your contact lists",
+        ]);
+
+        $user = User::where('email', $data['user_email'])->first(['id']);
+        $contact = Contact::where('email', $data['contact_email'])->first(['id']);
+
+        $shareExists = $contact->sharedWithUsers()->wherePivot('user_id', $user->id)->first();
+        if ($shareExists) {
+            return back()->withInput($request->all())->withErrors([
+                'contact_email' => "This contact was already shared with user $user->email",
+            ]);
+        }
+        $contact->sharedWithUsers()->attach($user->id);
+
+        return redirect()->route('home')->with('alert', [
+            'message' => "Contact $contact->email shared with $user->email succesfully",
+            'type' => 'succes',
+        ]);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,9 @@
                             @endif
                         @else
                             <li class="nav-item">
+                                <a class="nav-link" href="{{ route('contact-shares.create') }}">Share Contacts</a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('contacts.index') }}">My Contacts</a>
                             </li>
                             <li class="nav-item">
